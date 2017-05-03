ef527c201b2d89fa911a7ea060a50f928ba6522b
Cral-Gates <zhangxia2013105@163.com>
2017-05-03 08:58:38 +0800

    封装AsyncStorage

---
+++ lifeStyle/utils/StorageUtil.js
@@ -0,0 +1,47 @@
+/**
+ * Created by Cral-Gates on 2017/5/2.
+ */
+'use strict';
+import {Component} from 'react';
+import {
+    AsyncStorage
+} from 'react-native';
+import JsonUtil from '../utils/JsonUtil'
+
+class StorageUtil extends Component {
+    /*
+     * 获取
+     * */
+    static get(key) {
+        return AsyncStorage.getItem(key).then((value) => {
+            const jsonValue = JsonUtil.strToJson(value);
+            return jsonValue;
+        })
+    }
+
+    /*
+     * 保存
+     * */
+    static save(key, value, callback) {
+        return AsyncStorage.setItem(key, JsonUtil.jsonToStr(value), callback);
+    }
+
+    /*
+     * 更新
+     * */
+    static update(key, value) {
+        StorageUtil.get(key).then((item) => {
+            value = typeof value === 'string' ? value : Object.assign({}, item, value);
+            return AsyncStorage.setItem(key, JSON.stringify(value));
+        })
+    }
+
+    /*
+     * 删除
+     * */
+    static delete(key, callback) {
+        AsyncStorage.removeItem(key, callback);
+    }
+}
+
+export default StorageUtil;
